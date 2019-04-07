3f2be71aa1139cb54838c1de8ca79e969a2ebd72
ThaleS_CS <th4les.castro@gmail.com>
2019-04-07 11:36:05 -0300

    Inserido: Swagger atualizado

--- src/main/java/com/produtos/apirest/resources/ProdutoResource.java
+++ src/main/java/com/produtos/apirest/resources/ProdutoResource.java
@@ -4,6 +4,7 @@ package com.produtos.apirest.resources;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,36 +17,46 @@ import org.springframework.web.bind.annotation.RestController;
 import com.produtos.apirest.models.Produto;
 import com.produtos.apirest.repository.ProdutoRepository;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
 @RestController
 @RequestMapping(value="/api")
+@Api(value="API REST de Produtos")
+@CrossOrigin(origins="*")/*Qualquer dominio pode acessar a api...*/
 public class ProdutoResource {
 
 	@Autowired
 	ProdutoRepository produtoRepository;
 	
 	@GetMapping("/produtos")
+	@ApiOperation(value="Retorna uma Lista de todos os produtos do Banco de Dados")
 	public List<Produto> listaProdutos(){
 		return produtoRepository.findAll();
 	}
 	
 	@GetMapping("/produto/{id}")
+	@ApiOperation(value="Retorna um produto pelo seu id.")
 	public Produto listaProdutoUnico(@PathVariable(value="id") long id){
 		return produtoRepository.findById(id);
 	}
 	
 	@PostMapping("/produto")
+	@ApiOperation(value="Salva um produto no Banco de Dados")
 	public Produto salvaProduto(@RequestBody Produto produto) {
 		return produtoRepository.save(produto);
 	}
 	
 	
 	@DeleteMapping("/produto")
+	@ApiOperation(value="Deleta um produto do Banco de Dados")
 	public void deletaProduto(@RequestBody Produto produto) {
 		produtoRepository.delete(produto);
 	}
 	
 	
 	@PutMapping("/produto")
+	@ApiOperation(value="Atualiza um produtos do Banco de Dados")
 	public Produto atualizaProduto(@RequestBody Produto produto) {
 		return produtoRepository.save(produto);
 	}
